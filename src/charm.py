3491e2749d4423397cfc29e05538f8bdd71243e1
James Simpson <james.simpson@canonical.com>
2022-09-03 09:49:23 +0800

    VIII

--- src/charm.py
+++ src/charm.py
@@ -2,7 +2,7 @@
 
 # Copyright 2022 Canonical Ltd.
 # Licensed under the GPLv3, see LICENCE file for details.
-# Marker - VII
+# Marker - VIII
 
 """Charm for Indico on kubernetes."""
 import logging
